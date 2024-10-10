e4dd85a8dcdc4a17ccb64e27efeb14b0996c46e9
MerveAyakdaş <m.z.ayakdas@gmail.com>
2024-10-10 23:59:40 +0300

    Day2

---
+++ Day2/script.js
@@ -0,0 +1,13 @@
+function isPrime(n) {
+    if (n <= 1) return false;
+
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) return false; 
+    }
+
+    return true; 
+  }
+
+  console.log(isPrime(4));
+  console.log(isPrime(7));
+  console.log(isPrime(19));
